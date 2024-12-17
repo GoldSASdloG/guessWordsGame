3fa88027e557c32cabf365a3a14910ffba1fff6c
GoldSASdloG <Procatulus@gmail.com>
2024-12-17 11:17:47 +0300

    loadWords - ok

--- src/main/java/org/example/DataLoader.java
+++ src/main/java/org/example/DataLoader.java
@@ -1,23 +1,27 @@
 package org.example;
-import com.sun.tools.javac.util.List;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataLoader {
     public static final String WORDS_PATH_NAME = "src/main/resources/words.txt";
 
-    List<String> words = new ArrayList<>();
-    try {
-        Scanner scanner = new Scanner(new File(WORDS_PATH_NAME));
+    public static List<String> loadWords () {
 
-        while (scanner.hasNext()){
-            words.add(scanner.nextLine());
+        List<String> words = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(WORDS_PATH_NAME))){
+            while (scanner.hasNext()) {
+                words.add(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found exception!!!");
         }
-    } catch (FileNotFoundException e){
-        System.out.println("File not found exception!!!");
+        return words;
     }
+
+    List words = loadWords();
     Scanner keyboard = new Scanner(System.in);
 
 }
